fe9b4525bd4b64b4ba87c1c28b09a0d69ce935cc
Shushpancheak <Makcieml@gmail.com>
2019-02-16 20:41:04 +0300

    Input options

--- backup.sh
+++ backup.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+while getopts "e:n:p:" opt; do
+	case $opt in
+		e)
+			echo "Extension!"
+		;;
+
+		n)
+			echo "Name!"
+		;;
+
+		p)
+			echo "Path!"
+		;;
+
+		\?)
+			echo "Invalid option: -$OPTARG"
+		;;
+	esac
+done
+
+
 echo "Done."
